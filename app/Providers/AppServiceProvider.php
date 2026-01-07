40033ed400de7769d1d90473386fe78287f52d8a
ayundhaclaudia <ayundhacrappan@gmail.com>
2026-01-07 17:39:18 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // === SOLUSI PERMANEN AZURE ===
+        
+        // 1. Memaksa Laravel membuat link HTTPS
+        URL::forceScheme('https');
+
+        // 2. Memanipulasi Request agar Laravel sadar ini HTTPS
+        // (Penting untuk mengatasi Error 419 Page Expired)
+        if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'off') {
+            unset($_SERVER['HTTPS']);
+        }
+        
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+            $_SERVER['HTTPS'] = 'on';
+        }
+
+        $this->app['request']->server->set('HTTPS', 'on');
     }
 }
